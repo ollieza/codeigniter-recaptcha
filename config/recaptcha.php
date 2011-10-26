dc5556c34ccbd57cd142c0edf1f7edd785b159b8
Ollie Rattue <orattue@toomanytabs.com>
2011-10-26 19:09:03 +0100

    Added filename opening comment and aligned array in config file

--- config/recaptcha.php
+++ config/recaptcha.php
@@ -1,16 +1,18 @@
-<?
-/*
-The reCaptcha server keys and API locations
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-Obtain your own keys from:
-http://www.recaptcha.net
-*/
+// ------------------------------------------------------------------------
+// Recaptcha class config
+// ------------------------------------------------------------------------
+
+// The reCaptcha server keys and API locations
+// Obtain your own keys from: http://www.recaptcha.net
+
 $config['recaptcha'] = array(
-  'public'=>'YOUR PUBLIC KEY',
-  'private'=>'YOUR PRIVATE KEY',
-  'RECAPTCHA_API_SERVER' =>'http://www.google.com/recaptcha/api',
-  'RECAPTCHA_API_SECURE_SERVER'=>'https://www.google.com/recaptcha/api',
-  'RECAPTCHA_VERIFY_SERVER' =>'www.google.com',
-  'RECAPTCHA_SIGNUP_URL' => 'https://www.google.com/recaptcha/admin/create',
-  'theme' => 'red'
-);
+  							'public'						=> 'YOUR PUBLIC KEY',
+  							'private'						=> 'YOUR PRIVATE KEY',
+  							'RECAPTCHA_API_SERVER' 			=> 'http://www.google.com/recaptcha/api',
+  							'RECAPTCHA_API_SECURE_SERVER'	=> 'https://www.google.com/recaptcha/api',
+  							'RECAPTCHA_VERIFY_SERVER' 		=> 'www.google.com',
+  							'RECAPTCHA_SIGNUP_URL' 			=> 'https://www.google.com/recaptcha/admin/create',
+  							'theme' 						=> 'red'
+							);
